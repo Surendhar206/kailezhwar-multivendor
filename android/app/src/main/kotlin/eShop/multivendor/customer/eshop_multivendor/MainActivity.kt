965cba70ff917b731eed539869c6d51d780da3f9
Surendhar206 <royalar321@gmail.com>
2022-10-09 17:33:00 +0530

    message

--- android/app/src/main/kotlin/eShop/multivendor/customer/eshop_multivendor/MainActivity.kt
+++ android/app/src/main/kotlin/eShop/multivendor/customer/eshop_multivendor/MainActivity.kt
@@ -1,4 +1,4 @@
-package eShop.multivendor.customer
+package com.rscustomer.multivendor
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
